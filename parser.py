a57ac1d1356e6117e1871f941946d38e0cf0835a
cra01441 <monlyme671@gmail.com>
2023-02-20 23:02:48 +0300

    cringe

--- parser.py
+++ parser.py
@@ -11,55 +11,49 @@ def get_data(url):
         "User-Agent": ua.random
     }
 
-    for links in range(1, 4):
-        req = requests.get(url + f'?page={links}', headers)
-
-    with open('cloth.html', 'w') as file:
-        file.write(req.text)
-
-    with open('cloth.html') as file:
-        src = file.read()
-
-    soup = BeautifulSoup(src, 'lxml')
-    block = soup.find_all('li', class_='producto col-md-3 col-lg-3 col-sm-4 col-xs-6')
-
-    products = []
-    links_count = 3
+    links_count = 5
     print(f'Всего страниц {links_count}')
 
-    for i in block:
-        title_find = i.find('div', class_='name noOpt')
-        title=(title_find.text.replace('\n', '')) # название продукта
+    for links in range(0, 6):
+        req = requests.get(url + f'?page={links}', headers=headers)
 
-        price_find = i.find('div', class_='price row').find('span', class_='curent noOpt')
-        price=(price_find.text.replace('\n','')) # цена продукта
+        with open(f'cloth_{links}.html', 'w') as file:
+            file.write(req.text)
 
-        link_find = i.find('div', class_='name noOpt').find('a').get('href')
-        link=('https://issaplus.com'+link_find) # ссылка продукта
+        with open(f'cloth_{links}.html') as file:
+            src = file.read()
 
-        links_count -= 1
-        print(f'Страница #{links} завершена. Осталось страниц {links_count}')
+        soup = BeautifulSoup(src, 'lxml')
+        block = soup.find_all('li', class_='producto col-md-3 col-lg-3 col-sm-4 col-xs-6')
 
-        if links_count ==0:
-            print('Парсинг страниц закончен')
-            break
+        products = []
 
-        time.sleep(random.randrange(2, 4))
+        for i in block:
+            title_find = i.find('div', class_='name noOpt')
+            title=(title_find.text.replace('\n', '')) # название продукта
 
-        products.append(
-            {
-                'Title': title,
-                'Price': price,
-                'Link': link,
-                # 'Discount': discount or nodiscount
-            }
-        )
+            price_find = i.find('div', class_='price row').find('span', class_='curent noOpt')
+            price=(price_find.text.replace('\n','')) # цена продукта
+
+            link_find = i.find('div', class_='name noOpt').find('a').get('href')
+            link=('https://issaplus.com'+link_find) # ссылка продукта
+
+            products.append(
+                {
+                    'Title': title,
+                    'Price': price,
+                    'Link': link,
+                    # 'Discount': discount or nodiscount
+                }
+            )
+
+            # time.sleep(random.randrange(2, 4))
 
         # print(title+'\n'+price+'\n'+link+'\n') # вывод данных
 
-    with open('data.json', 'w', encoding='utf=8') as jfile:
+    with open(f'data_{links}.json', 'w', encoding='utf=8') as jfile:
         json.dump(products, jfile, indent=4, ensure_ascii=False)
-
+print('done')
 get_data("https://issaplus.com/odezhda/")
 
 
